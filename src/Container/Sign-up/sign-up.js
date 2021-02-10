df93abcf7e89419d8feb5eabbd8c8823bb268687
omarhajara <umarhajar3@gmail.com>
2021-02-10 12:45:48 -0800

    sign-in sign-up lecturers-list-card

---
+++ src/Container/Sign-up/sign-up.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./sign-up.scss";
+import Vector from "../../Assets/Vector.png";
+
+function SignUp() {
+  return (
+    <div className="sign-up">
+      <div className="container">
+        <h1 className="sign-in-up-header text-center">Register</h1>
+        <form action="">
+          <div className="input-field">
+            <input type="text" name="name" placeholder="Full Name" />
+          </div>
+          <div className="input-field">
+            <input type="email" name="email" placeholder="johndoe@gmail.com" />
+          </div>
+          <div className="input-field">
+            <input type="tel" name="phone" placeholder="Phone Number" />
+          </div>
+          <div className="input-field">
+            <input type="text" name="matric-no" placeholder="Matric No" />
+          </div>
+          <div className="input-field">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password"
+            />
+            <img src={Vector} alt="" />
+          </div>
+          <div className="input-field">
+            <input
+              type="password"
+              name="password"
+              placeholder="Comfirm Password"
+              id="password"
+            />
+            <img src={Vector} alt="" />
+          </div>
+          <div className="submit">
+            <input type="submit" name="submit" value="create account" />
+          </div>
+        </form>
+        <p className="text-center">
+          Already have an account? <a className="last-child">Sign up here</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;

3e0445af795323b23237101686d6a18e858e469e
Tamimia <tamigags@gmail.com>
2021-02-12 16:00:46 +0200

    flow

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./footer.scss";
+import Home from "../../assets/images/home.png";
+
+function Footer() {
+  return (
+    <div className="footerContent">
+      <div className="body"></div>
+      <div className="homeIcon">
+        <img src={Home} alt="home-icon" />
+        <img src={Home} alt="home-icon" />
+        <img src={Home} alt="home-icon" />
+        <img src={Home} alt="home-icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Homepage/homePage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./homePage.scss";
+import Photo from "../../assets/images/half.png";
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="appContainer">
+        <div className="homeImage">
+          <img src={Photo} alt="pix" />
+        </div>
+        <div className="paragraph">
+          <h2>Level Up!</h2>
+          <p>
+            Top lecturers from different universities, teaching millions of
+            students on uLectures.
+          </p>
+        </div>
+      </div>
+
+      <div className="container">
+        <h1>uLectures</h1>
+        <button type="btn">Start Learning</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
